0ec99e9cbdaf43fc244aca4707a9607fc3b3c4e6
Travis <travis.x.wong@gmail.com>
2020-02-21 09:02:17 +0800

    update

---
+++ canvas.js
@@ -0,0 +1,127 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var eraserEnable = false;
+autoSize();
+
+listenToMouse();
+
+listenToActions();
+
+function listenToActions(){
+  eraser.onclick = function(){
+  eraserEnable = true;
+  console.log(eraserEnable)
+  }
+  brush.onclick = function(){
+  eraserEnable = false;
+  }
+}
+
+function listenToMouse(){
+  var drawFlag = false
+  var lastPosition ={}
+  lastPosition.x2 = 0;
+  lastPosition.y2 = 0;
+  if (document.body.ontouchstart !== undefined){
+    canvas.ontouchstart = function(a){
+      console.log('开始摸我了')
+      console.log(eraserEnable)
+      var x = a.touches[0].clientX;
+      var y = a.touches[0].clientY;
+      drawFlag = true
+      if(eraserEnable){
+        context.clearRect(x-5,y-5,10,10);
+        }else{
+          lastPosition.x2 = x;
+          lastPosition.y2 = y;
+        }
+    }
+
+    canvas.ontouchmove = function(a){
+      console.log('啊，雅蠛蝶')
+      var x = a.touches[0].clientX;
+      var y = a.touches[0].clientY;
+    
+      if (!drawFlag){return}
+    
+      if(eraserEnable){
+        context.clearRect(x-5,y-5,10,10)
+        }else{
+          drawLine(x,y,lastPosition.x2,lastPosition.y2);
+          lastPosition.x2 = x;
+          lastPosition.y2 = y;
+        }
+    }
+
+    canvas.ontouchend = function(){
+      console.log('不要停')
+      drawFlag = false
+    }
+
+  }else{
+    canvas.onmousedown = function(a) {
+      console.log(eraserEnable)
+      var x = a.clientX;
+      var y = a.clientY;
+      drawFlag = true
+      if(eraserEnable){
+        context.clearRect(x-5,y-5,10,10);
+        }else{
+          lastPosition.x2 = x;
+          lastPosition.y2 = y;
+        }
+      }
+    
+     canvas.onmousemove = function(a) {
+      var x = a.clientX;
+      var y = a.clientY;
+    
+      if (!drawFlag){return}
+    
+      if(eraserEnable){
+        context.clearRect(x-5,y-5,10,10)
+        }else{
+          drawLine(x,y,lastPosition.x2,lastPosition.y2);
+          lastPosition.x2 = x;
+          lastPosition.y2 = y;
+        }
+      }
+    
+      canvas.onmouseup = function(a) {
+        drawFlag = false
+      }
+      
+  }
+  
+}
+
+
+function drawCircle(x,y,r) {
+  context.beginPath();
+  context.arc(x,y,r,0,Math.PI*2,true);
+  context.fill();
+}
+
+function drawLine(x1,y1,x2,y2){ 
+  context.beginPath();
+  context.moveTo(x1,y1);
+  context.lineTo(x2,y2);
+  context.lineWidth = 2;
+  context.stroke();
+}
+
+// 获取窗口宽高
+function getSize(){
+  var pageWidth = document.documentElement.clientWidth;
+  var pageHeight = document.documentElement.clientHeight;
+
+  canvas.width = pageWidth
+  canvas.height = pageHeight
+}
+
+function autoSize() {
+  getSize()  //获取窗口宽高
+  window.onresize = function() {
+    getSize()
+  }
+}
